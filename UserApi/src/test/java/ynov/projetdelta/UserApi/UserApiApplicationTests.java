613c6cd3f6c34ded5c89b7e592e25cb38a8e322d
matthiasdeschatrettes <72861966+matthiasdeschatrettes@users.noreply.github.com>
2020-12-14 09:45:39 +0100

    Add files via upload

---
+++ UserApi/src/test/java/ynov/projetdelta/UserApi/UserApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ynov.projetdelta.UserApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
