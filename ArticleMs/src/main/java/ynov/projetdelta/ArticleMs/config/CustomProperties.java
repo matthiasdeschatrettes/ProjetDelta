86310582f3b5301891dfc74af84a90e993ad0c59
matthiasdeschatrettes <72861966+matthiasdeschatrettes@users.noreply.github.com>
2021-01-04 17:48:37 +0100

    Add files via upload

---
+++ ArticleMs/src/main/java/ynov/projetdelta/ArticleMs/config/CustomProperties.java
@@ -0,0 +1,20 @@
+package ynov.projetdelta.ArticleMs.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "ynov.projetdelta")
+public class CustomProperties {
+
+	private String apiUrl;
+	
+	public String getApiUrl() {
+		return apiUrl;
+	}
+	
+	public void setApiUrl(String apiUrl) {
+		this.apiUrl = apiUrl;
+	}
+	
+}
