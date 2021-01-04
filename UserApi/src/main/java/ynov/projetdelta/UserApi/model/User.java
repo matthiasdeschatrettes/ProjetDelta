5873a5390c1dfb637244cfc4fd6f954f4263087a
matthiasdeschatrettes <72861966+matthiasdeschatrettes@users.noreply.github.com>
2021-01-04 18:34:58 +0100

    Add files via upload

--- UserApi/src/main/java/ynov/projetdelta/UserApi/model/User.java
+++ UserApi/src/main/java/ynov/projetdelta/UserApi/model/User.java
@@ -1,10 +1,28 @@
 package ynov.projetdelta.UserApi.model;
 
-public class User {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "users")
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id")
 	private Integer id;
+	
+	@Column(name ="username")
 	private String username;
+	
+	@Column(name ="password")
 	private String password;
+	
+	@Column(name ="email")
 	private String email;
 	
 	public Integer getId() {
--- UserApi/src/main/java/ynov/projetdelta/UserApi/repositories/UserRepository.java
+++ UserApi/src/main/java/ynov/projetdelta/UserApi/repositories/UserRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import ynov.projetdelta.UserApi.model.User;
-import ynov.projetdelta.UserApi.repositories.UserRepository;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, Integer> {
